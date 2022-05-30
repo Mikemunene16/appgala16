12f211e332247397472d642ed1e8e2897c526d7b
Mike Munene <mike.munene@moringaschool.com>
2022-05-30 22:25:44 +0300

    Create views and templates

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    title = "Welcome to appgala, showcasing galleries"
+    return render(request, 'main/index.html', locals())
+
+def hello(request):
+    title = "Hello there, 2nd page works"
+    return render(request, 'main/hello.html', locals())
