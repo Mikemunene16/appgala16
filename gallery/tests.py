35407c73eb47218b35fd95afb88aa7a302e04765
Mike Munene <mike.munene@moringaschool.com>
2022-06-01 21:14:18 +0300

    Run test on category model methods

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
+from .models import Category, Location, Image
+
 # Create your tests here.
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.category = Category(category_name='Best Sceneries')
+        self.category.save_category()
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.category, Category))
+
+    def test_save_category(self):
+        self.category.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories) > 0)
+
+    def test_delete_category(self):
+        self.category.delete_category()
+        category = Category.objects.all()
+        self.assertTrue(len(category) == 0)
